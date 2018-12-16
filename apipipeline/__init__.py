c3a7129c0767694925fb768b7bfab6b886d46b32
genprog_server <support@generalprogramming.org>
2018-12-16 00:52:14 +0000

    Flask integration begone.

--- apipipeline/__init__.py
+++ apipipeline/__init__.py
@@ -5,8 +5,6 @@ sentry_sdk = None
 
 if "SENTRY_DSN" in os.environ:
     import sentry_sdk
-    from sentry_sdk.integrations.flask import FlaskIntegration
-    from sentry_sdk.integrations.celery import CeleryIntegration
 
     sentry_sdk.init(
         dsn=os.environ["SENTRY_DSN"],
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-COMPOSEFILE="${0%/*}/../docker-compose.yml"
+COMPOSEFILE="${0%/*}/../docker-compose-server.yml"
 echo $SCRIPTDIR
 
 docker-compose -f "$COMPOSEFILE" build --force-rm
